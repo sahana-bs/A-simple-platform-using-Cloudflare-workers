1ded6ea17006948f8602d439c26ec7e22978ed85
Sahana Basapathi <srsahana67@gmail.com>
2022-01-31 22:04:19 -0500

    added new posts

--- workers/serverless-api-3/src/posts_store.ts
+++ workers/serverless-api-3/src/posts_store.ts
@@ -2,13 +2,13 @@ const _posts = [
   {
     id: 1,
     title: "My first blog post",
-    text: "Hello world! This is my first blog post on my new Cloudflare Workers + Pages blog.",
+    text: "Hi, this is pretty cool to build a simple platform using cloudflare, I had fun!!!.",
     published_at: new Date("2022-01-31")
   },
   {
     id: 2,
-    title: "Updating my blog",
-    text: "It's my second blog post! I'm still writing and publishing using Cloudflare Workers + Pages :)",
+    title: "Maybe a lil about me",
+    text: "I am doing my Masters from Virginia Tech and loving it so far. Something about me that's not on my resume,\nI am a super curious person and love learning and challenging myself. I read a lot on spirituality\nand love to keep myself positive and energised :) ",
     published_at: new Date("2022-01-31")
   }
 ]
